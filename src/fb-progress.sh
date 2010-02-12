fcf8829ef9541fcef1c7ed6dda83942b9001d58b
Kimmo Hämäläinen <kimmo.hamalainen@nokia.com>
2010-02-12 11:12:45 +0200

    use /tmp for fb-progress.pid

--- src/fb-progress.sh
+++ src/fb-progress.sh
@@ -16,7 +16,7 @@
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
 # 02110-1301 USA
 
-PIDFILE=$AF_PIDDIR/fb-progress.pid
+PIDFILE=/tmp/fb-progress.pid
 BAR_IMGDIR=/usr/share/icons/hicolor/scalable/hildon
 BAR=indicator_update
 
